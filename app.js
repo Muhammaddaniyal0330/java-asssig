c9ea5bf264f64a2992f8984172225309a86cdfc7
Muhammaddaniyal0330 <daniyalcsonly40@gmail.com>
2024-03-03 14:37:01 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,456 @@
+// ---- CHAPTER 1-10 -------- // 
+
+// Q1 
+
+// var city = 'karachi';
+
+// if(city === 'karachi'){
+//     console.log('THE CITY OF LIGHT');
+// }
+
+// // Q2 
+
+// var y = 10;
+// var x = 10;
+
+// if(x === y ){
+//     var z = +prompt('THE VALUE OF Z IS')
+//     var store = z;
+//     console.log(store);
+// }
+
+// Q3 
+
+// var zipCode = 10010;
+
+// if(zipCode === 10010){
+//     alert('karachi')
+// }else{
+//     alert('incorrect code?')
+// }
+
+
+// ----- CHAPTER 11 ------ // 
+
+// Q1
+
+// var num = 5;
+// var num2 = 4;
+
+// if(num != num2){
+//     console.log('EQUAL');
+// }else{
+//     console.log(' NOT EQUAL');
+// }
+
+
+// Q2 
+
+// var a = 24;
+// var b = 24;
+
+// if(a >= b){
+//     console.log('a is greater than equal b');
+// }else{
+//     console.log('a is not greater than equal b');
+// }
+
+
+// Q3 
+
+// var vari = 10;
+
+// if(vari != 5){
+//     var vari = 5;
+//     console.log(vari)
+// }
+
+// Q4 
+
+// var num = 10;
+// var num2 = 5;
+
+// if(num != num2){
+//     alert('congratulations ')
+// }
+
+// Q5 
+
+// var userName = 'muhammad daniyal';
+// var userPro = prompt('ENTER YOUR NAME')
+
+// if(userName != userPro){
+//     alert('name not found')
+// }
+
+
+// ----- CHAPTER 12 ------- //
+
+// Q1 
+// var num = 5;
+// var num2 = 5;
+
+// if(num => num2){
+//     alert('EQUAL')
+// }else{
+//     alert('NOT EQUAL')
+// }
+
+// Q2 
+
+// var obtain = +prompt('ENTER YOUR OBTAIN MARKS')
+// var total = +prompt('ENTER YOUR TOTAL MARKS')
+// var cal = (obtain/total)*100
+
+// if(cal >= 80){
+//     alert('YOU GOT A+')
+// }else if(cal >= 70) {
+//     alert('YOU GOT A')
+// }else if(cal >= 60){
+//     alert('YOU GOT B')
+// }else if(cal >= 50){
+//     alert('YOU GOT B+')
+// }else if(cal >= 40){
+//     alert('YOU GOT C')
+// }else if(cal >= 33){
+//     alert('YOU GOT F')
+// }else{
+//     alert('INVALID CODE')
+// }
+
+
+// Q3
+
+// var a = 10;
+
+// if(a===120){
+//     alert(' the value of A is 10')
+// }else{
+//     alert('the correct value of A is ' + a)
+// }
+
+// Q14
+
+// var userPro = prompt('ENTER YOUR CITY')
+
+// if(userPro === 'karachi'){
+//     alert("It's is karachi")
+// }else if(userPro === 'lohore'){
+//     alert("It's is lohore")
+// }else{
+//     alert("Enter valid city name")
+// }
+
+
+
+// ------- CHAPTER 14 -------- // 
+
+// Q1
+
+// var a = 5;
+// var b = 5;
+
+// c = 10;
+// d = 10;
+
+// if (a===b && c===d){
+//     alert('both are True')
+// }else{
+//     alert('both are not True')
+// }
+
+
+// Q2 
+
+// var a = 5;
+// var b = 5;
+
+// c = 10;
+// d = 12;
+
+// if (a===b && c===d){
+//     alert('both are True')
+// }else{
+//     alert('both are not True')
+// }
+
+
+// Q3 
+
+// var persone1 = 'hamza';
+// var persone2 = 'arsalan';
+
+// if(persone1 <= 60 && persone2 <= 60){
+//     alert('AGE IS GREATER THAN 60')
+// }else{
+//     alert('AGE IS GREATER THEN 60')
+// }
+
+
+// Q4 
+
+// var fristVari = 9;
+// var secondVari = 10;
+
+// if(fristVari <= secondVari || fristVari >= secondVari){
+//         alert("The first variable is either less than or greater than the second variable.");
+
+// }
+
+
+// Q5 
+
+
+// var fristName = 'dani';
+// var secondName = 'sidd';
+
+// var userPro = prompt('ENTER YOUR FIRST NAME');
+// var userPro2 = prompt('ENTER YOUR SECOND NAME');
+
+// if(userPro === fristName && userPro2 === secondName){
+//     alert('NAME MATCH')
+// }else{
+//     alert('NOT MATCH')
+// }
+
+// ----- CHAPTER 14 ------- //
+
+// Q1
+// var password = prompt("Please enter your password:");
+
+// if (password !== "") {
+//     if (password.length <= 5) {
+//         alert("Password must be greater than 5");
+//     } else {
+//         alert("Your Password is OK");
+//     }
+// }
+
+// Q2 
+
+// var a = 1;
+// var c = 'max'
+
+// if (a === 1){
+//     if (c === 'max') {
+//         alert('ok');
+//     }
+// }
+
+// Q3 
+
+// var a = 1;
+// var c = 'max';
+
+// if (a === 1 && c === "max") {
+//     alert("OK");
+// }
+
+// ------- CHAPTER 9-11 ---------- //
+
+// Q1
+
+// var city = 'karachi';
+// var cityPro = prompt('ENTER YOUR CITY');
+
+// if(cityPro === city){
+//     alert('WELCOME TO THE CITY OF LIGHT')
+// }
+
+// Q2
+
+
+// var gender = prompt('ENTER YOUR GENDER')
+
+
+// if (gender == 'male') {
+//     alert('WELCOME SIR')
+//     if (gender == 'female') {
+//     alert('WELCOME MAHAM')
+//     }
+// }
+
+// Q3 
+
+// var sigPro = prompt('ENTER YOUR COLOUR');
+
+// if(sigPro == 'red'){
+//     alert('STOP');
+// }else if(sigPro == 'yellow'){
+//     alert('READY TO GO')
+// }else if(sigPro == 'green'){
+//     alert('MOVE NOW')
+// }else{
+//     alert('invalid code')
+// }
+
+// Q4 
+
+// var userPro = prompt('ENTER YOUR FUAL');
+
+// if(userPro <= 0.25){
+//     alert('Please refill the fuel in your car')
+// }
+
+
+// Q5
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83) {
+//     alert("given condition for variable b is true");
+// }
+
+// var c = 12;
+// if (c++ === 13) {
+//     alert("condition 1 is true");
+// }
+
+// if (c === 13) {
+//     alert("condition 2 is true");
+// }
+
+// if (c < 14) {
+//     alert("condition 3 is true");
+// }
+
+// if(c === 14){
+//     alert("condition 4 is true");
+// }else{
+//     alert('not true')
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost) {
+//     alert("The cost equals");
+// }
+
+
+// if (true) {
+//     alert("True");
+// }
+// if (false) {
+//     alert("False");
+// }
+
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+
+// Q8
+
+// var userPro = +prompt('ENTER YOUR NUMBER')
+
+// if(userPro % 3 === 0 ){
+//     alert('divided by 3')
+// }else{
+//     alert('not devided')
+// }
+
+// Q9 
+
+// var input = 8;
+
+// if(input % 2 == 0){
+//     alert('Even')
+// }else{
+//     alert('Odd')
+// }
+
+
+// Q10
+
+// var userTemp = +prompt('ENTER YOUR TEMP');
+
+// if(userTemp > 40){
+//     alert("It is too hot outside.")
+// }else if(userTemp > 30){
+//     alert("Weather is normal")
+// }else if(userTemp > 20){
+//     alert('Weather is cool')
+// }else if(userTemp > 10){
+//     alert('Weather is so cool')
+// }
+
+
+// Q11
+
+// var user1 = +prompt('ENTER YOUR FIRST NUMBER');
+// var opera = prompt('ENTER YOUR OPERATOR');
+// var user2 = +prompt('ENTER YOUR SECOND NUMBER');
+
+// if(opera === '+'){
+//     alert(user1 +  user2)
+// }else if(opera === '-'){
+//     alert(user1 - user2)
+// }else if(opera === '/'){
+//     alert(user1 / user2)
+// }else if(opera === '*'){
+//     alert(user1 * user2)
+// }
+// else if(opera === '%'){
+//     alert(user1 % user2)
+// }else{
+//     alert('invalid code')
+// }
+
+// --------- CHAPTER 11 -12 ---------- //
+
+// Q2 
+
+// var inte1 = 50;
+// var inte2 = 30;
+
+// if(inte1 > inte2){
+//     console.log('integer 1 is greater');
+// }else if(inte2 > inte1){
+//     console.log('integer 2 is greater');
+// }else{
+//     console.log('something wrong');
+// }
+
+// Q3
+
+// var userPro = prompt('ENTER YOUR INPUT');
+
+// if(userPro > 0){
+//     alert('POSITIVE NUMBER')
+// }else if(userPro < 0){
+//     alert('negative number')
+// }else if(userPro == 0){
+//     alert('number is zero')
+// }else{
+//     alert('invalid code')
+// }
+
+
+// Q5
+
+// var password = "muhammaddaniyal947";
+// var userPro = prompt('ENTER YOUR PASSWORD')
+
+// if(!password){
+//     alert('PLS ENTER YOUR PASSWORD')
+// }else if(password === userPro){
+//     alert('correct password')
+// }else{
+//     alert('password not match')
+// }
+
+
+// Q6
+
+// var greeting = '';
+// var hour = 14;
+
+// if(hour <= 13){
+//    alert( greeting = 'Good day');
+// }else{
+//     alert(greeting = 'good evening')
+// }
